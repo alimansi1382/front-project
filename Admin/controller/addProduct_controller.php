c3fc1a3014b3b6dd147c477bab4508edb1953f8e
alimansi1382 <92879191+alimansi1382@users.noreply.github.com>
2022-01-04 15:57:10 +0330

    Add files via upload

--- Admin/controller/addProduct_controller.php
+++ Admin/controller/addProduct_controller.php
@@ -1,65 +1,91 @@
 <?php
 require_once("__init__.php");
 
-if ($_SESSION['role']===1) {
-    if (!isset($_POST['submit'])) 
-    //View
-    {
-        include_once('../views/addProduct_view.php');
-    }
-    else 
-    //Controller
-    {
-        $name_servic = $_POST['name_servic'];
-        $price_servic = $_POST['price_servic'];
-        $icon_servic = $_POST['icon_servic'];
-
-        // Add Product
-        $cdb = new db($dbhost,$dbusername,$dbpassword,$dbname);
-        $query = "INSERT INTO 
-        product(name,catg,price_component,price,image_src)
-        VALUES('{$_POST['product_name']}','{$_POST['product_catg']}','{$_POST['price_component']}',{$_POST['product_price']},'{$_POST['product_image']}')";
-        $result = $cdb -> query($query);
-        if ($cdb -> error) {
-            echo $cdb -> error;
-        } else {
-            echo '<div class="success-alert">موفقیت امیز</div>';
-        }
-        $cdb -> close();
-
-        $uploads_dir = '/assets/images';
-        // foreach ($_FILES["product_image"]["name"] as $key => $image) {
-
-        //     $tmp_name = $_FILES["product_image"]["tmp_name"][$key];
-        //     $name = basename($_FILES["product_image"]["name"][$key]);
-        //     move_uploaded_file($tmp_name, "$uploads_dir/{$name}");
-        // }
-
-        // get Product Id
-        $cdb = new db('localhost','root','','frontproject');
-        $query = "SELECT id FROM product WHERE name = ?";
-        $result = $cdb -> query($query,$_POST['product_name'])->fetchArray();
-        $productId = $result['id'];
-        print_r($result);
-
-        //Add Services
-        for ($i=0; $i < count($name_servic); $i++) { 
-            if (!$name_servic[$i]) {
-                break;
-            }
-            $service_query = "INSERT INTO 
-            services(name,price,product_id,image_src)
-            VALUES('{$name_servic[$i]}',{$price_servic[$i]},{$productId},'{$icon_servic[$i]}')";
-            $result = $cdb -> query($service_query);
-        }
-        if ($cdb -> error) {
-            echo $cdb -> error;
-        } else {
-            echo '<div class="success-alert">موفقیت امیز</div>';
-        }
-        $cdb -> close();
-    }
-}else {
-    echo "not access";
+if (!isset($_POST['submit'])) 
+//View
+{
+    include_once('../views/addProduct_view.php');
 }
+else 
+//Controller
+{
+    header("Location:{$controllerroot}listProduct_controller.php");
+    // $name_servic = $_POST['name_servic'];
+    // $price_servic = $_POST['price_servic'];
+    // $icon_servic = $_POST['icon_servic'];
 
+    // Add Product
+    $cdb = new db($dbhost,$dbusername,$dbpassword,$dbname);
+    $query = "INSERT INTO 
+    product(name,catg,price_component,price,image_src)
+    VALUES('{$_POST['product_name']}','{$_POST['product_catg']}','{$_POST['price_component']}',{$_POST['product_price']},'{$_FILES["product_image"]["name"]}')";
+    $result = $cdb -> query($query);
+    $cdb -> close();
+
+    
+    $target_dir = "../../assets/images/products/";
+    $target_file = $target_dir . basename($_FILES["product_image"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["product_image"]["tmp_name"]);
+    if($check !== false) {
+        $uploadOk = 1;
+    } else {
+        $uploadOk = 0;
+    }
+    }
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+    $uploadOk = 0;
+    }
+
+    // Check file size
+    if ($_FILES["product_image"]["size"] > 500000) {
+    $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+    $uploadOk = 0;
+    }
+
+    if ($uploadOk == 0) {
+    } else {
+        if (move_uploaded_file($_FILES["product_image"]["tmp_name"], $target_file)) {
+            
+        } else {
+        }
+    }
+
+    
+
+
+    // get Product Id
+    // $cdb = new db('localhost','root','','frontproject');
+    // $query = "SELECT id FROM product WHERE name = ?";
+    // $result = $cdb -> query($query,$_POST['product_name'])->fetchArray();
+    // $productId = $result['id'];
+    // print_r($result);
+
+    //Add Services
+    // for ($i=0; $i < count($name_servic); $i++) { 
+    //     if (!$name_servic[$i]) {
+    //         break;
+    //     }
+    //     $service_query = "INSERT INTO 
+    //     services(name,price,product_id,image_src)
+    //     VALUES('{$name_servic[$i]}',{$price_servic[$i]},{$productId},'{$icon_servic[$i]}')";
+    //     $result = $cdb -> query($service_query);
+    // }
+    // if ($cdb -> error) {
+    //     echo $cdb -> error;
+    // } else {
+    //     echo '<div class="success-alert">موفقیت امیز</div>';
+    // }
+    // $cdb -> close();
+}
--- Admin/views/Dashboard_view.php
+++ Admin/views/Dashboard_view.php
@@ -5,21 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>حساب کاربری</title>
-    <?php include"__init__.php"; ?>
+    <?php include "__init__.php"; ?>
 </head>
-<body id="body-container" class="admintPanel grid-20-80">
-    <!-- include Sidebar -->
-    <?php include "Sidebar.php"; ?>
-    <main id="main-container">
-        <!-- include Header -->
-        <?php include "Header.php"; ?>
-        <section id="container">
-            adfd
+<body id="body-container" class="admintPanel">
+    <?php include_once('Header.php'); ?>    
+    <main id="container" class="container">
+        <section class="grid-display col-sm-1 col-md-1 col-lg-3">
+            <?php include_once('Sidebar.php'); ?>
+            <section class="grid-lg-1to4">
+                <section class="page_content grid-display col-sm-1 col-md-2 col-lg-4">
+                    <div class="infosmallbox">
+                        <div class="infosmallbox_icon"><i class="fas fa-chart-bar"></i></div>
+                        <div class="infosmallbox_content">
+                            <h5 class="infosmallbox_content_head">سود ماه اخیر</h5>
+                            <h6 class="infosmallbox_content_value">360000 ریال</h6>
+                        </div>
+                    </div>
+                    <div class="infosmallbox">
+                        <div class="infosmallbox_icon"><i class="fas fa-box"></i></div>
+                        <div class="infosmallbox_content">
+                            <h5 class="infosmallbox_content_head">سفارشات ماه اخیر</h5>
+                            <h6 class="infosmallbox_content_value">36000</h6>
+                        </div>
+                    </div>
+                    <div class="infosmallbox">
+                        <div class="infosmallbox_icon"><i class="fas fa-chart-bar"></i></div>
+                        <div class="infosmallbox_content">
+                            <h5 class="infosmallbox_content_head">کاربران جدید</h5>
+                            <h6 class="infosmallbox_content_value">360</h6>
+                        </div>
+                    </div>
+                    <div class="infosmallbox">
+                        <div class="infosmallbox_icon"><i class="fas fa-chart-bar"></i></div>
+                        <div class="infosmallbox_content">
+                            <h5 class="infosmallbox_content_head">سود ماه اخیر</h5>
+                            <h6 class="infosmallbox_content_value">360000 ریال</h6>
+                        </div>
+                    </div>
+                </section>
+            </section>
         </section>
     </main>
-    <!-- <nav id="side-navigation"></nav> -->
-    <!-- <footer id="footer"></footer> -->
-    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
+    <?php include_once('__script__.php'); ?>
 </body>
 </html>
 
